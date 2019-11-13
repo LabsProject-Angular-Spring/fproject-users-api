02a1fc43139c7c868bc55d8452742762433cae38
andreslargo <felipeflogxd>
2019-11-13 11:30:13 -0500

    User Api feign

--- src/main/java/labsproject/api/controller/UserController.java
+++ src/main/java/labsproject/api/controller/UserController.java
@@ -3,18 +3,22 @@ package labsproject.api.controller;
 
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import labsproject.api.entity.User;
 import labsproject.api.service.IUserService;
 
 @RestController
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
+
 @RequestMapping(path="/user")
 public class UserController {
 	
@@ -64,7 +68,8 @@ public class UserController {
 		}			
 	}
 	@PostMapping(path="/login", consumes = {"application/json"})
-	public User Login (@RequestBody Map<String, Object> json) {			
+	public User Login (@RequestBody Map<String, Object> json) {		
+
 		return userService.Login(json.get("username").toString(), json.get("password").toString());			
 	}
 	
